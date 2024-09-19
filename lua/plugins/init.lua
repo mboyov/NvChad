42da1dd4c279692db07475fe91bf5dec9807c58e
v__nova <mboyov@arcadev.ch>
2024-09-19 17:44:00 +0200

    Tools Installation & Configuration
    - Setup Format and Style with Conform
    - Setup Treesitter
    - Setup LSPConfig
    - Setup Linting
    - Setup Mason-conform
    - Setup Mason-lspconfig
    - Setup Mason-lint

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,11 +17,45 @@ return {
     },
 
     {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require("configs.mason-lspconfig")
+        end,
+    },
+
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.lint")
+        end,
+    },
+
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require("configs.mason-lint")
+        end,
+    },
+
+    {
         "stevearc/conform.nvim",
         event = "BufWritePre", -- uncomment for format on save
         opts = require("configs.conform"),
     },
 
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require("configs.mason-conform")
+        end,
+    },
     -- These are some examples, uncomment them if you want to see them work!
     -- {
     --     "neovim/nvim-lspconfig",
