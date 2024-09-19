ff3fc8f0eb2bc67e87c2b631969ba74dbbe9eb29
v__nova <mboyov@arcadev.ch>
2024-09-19 14:51:10 +0200

    Base config
    Theme and space

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,11 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+
+-- Indenting
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,42 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("configs.treesitter")
+        end,
+    },
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require("configs.lspconfig")
+        end,
+    },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre", -- uncomment for format on save
+        opts = require("configs.conform"),
+    },
+
+    -- These are some examples, uncomment them if you want to see them work!
+    -- {
+    --     "neovim/nvim-lspconfig",
+    --     config = function()
+    --         require("configs.lspconfig")
+    --     end,
+    -- },
+
+    -- {
+    -- 	"nvim-treesitter/nvim-treesitter",
+    -- 	opts = {
+    -- 		ensure_installed = {
+    -- 			"vim", "lua", "vimdoc",
+    --      "html", "css"
+    -- 		},
+    -- 	},
+    -- },
 }
