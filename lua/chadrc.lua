b934f259bfcb5f31b7df318639a6ec7e8f643b4e
v__nova <mboyov@arcadev.ch>
2024-10-03 01:46:39 +0200

    update comments for better clarity in plugin configuration

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,18 +1,22 @@
--- This file needs to have same structure as nvconfig.lua
--- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
--- Please read that file to know all available options :(
+-- This file must follow the same structure as the nvconfig.lua file used by NvChad
+-- You can reference it here: https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please review that file to understand all the available options for customizing NvChad.
 
----@type ChadrcConfig
+---@type ChadrcConfig -- Defining the table type as ChadrcConfig for autocompletion and validation in Lua
 local M = {}
 
+-- Base46 configuration, which controls theme and highlights in NvChad
 M.base46 = {
-    theme = "catppuccin",
-    transparency = true,
+    theme = "catppuccin", -- Sets the color scheme to "catppuccin" (a popular, aesthetically pleasing theme)
+    transparency = true, -- Enables transparent background in the Neovim UI
 
+    -- Highlight override options:
+    -- hl_override allows you to manually override the default highlight settings
+    -- Uncomment and customize the following lines to apply different styles to comments
     -- hl_override = {
-    -- 	Comment = { italic = true },
-    -- 	["@comment"] = { italic = true },
+    --     Comment = { italic = true }, -- Makes comments italicized
+    --     ["@comment"] = { italic = true }, -- Applies the same for Treesitter comments
     -- },
 }
 
-return M
+return M -- Return the configuration table so that NvChad can apply it
--- lua/options.lua
+++ lua/options.lua
@@ -1,12 +1,15 @@
-require "nvchad.options"
+-- Load default options from NvChad's core configuration
+require("nvchad.options")
 
--- add yours here!
+-- You can add or override your own custom options below this line
 
-local o = vim.o
+local o = vim.o -- `vim.o` is used to set global options in Neovim
 
--- Indenting
-o.shiftwidth = 4
-o.tabstop = 4
-o.softtabstop = 4
+-- Indenting settings
+o.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+o.tabstop = 4 -- Number of spaces that a <Tab> character represents
+o.softtabstop = 4 -- Number of spaces that <Tab> counts for while editing (in insert mode)
 
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- Option to enable cursorline (shows a line highlighting the cursor position)
+-- Uncomment the following line to enable cursorline both on the line number and in the line text
+-- o.cursorlineopt = 'both' -- Use 'both' to highlight the cursor's row and column
